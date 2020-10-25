6a9aaaf09f456291574692156356c7b6371f4561
Lucas <lucsousan@gmail.com>
2020-10-25 14:11:36 -0300

    primeiro

---
+++ Java/java-vscode/maratonajava/introducao/teste.java
@@ -0,0 +1,12 @@
+// ter cuidado com caracteres especiais pq não roda legal, vai dar dor de cabeça e vc vai ficar muito irritado
+// cuidado
+
+package maratonajava.introducao; // primeiro informar o pacate aonde está o programa 
+
+public class teste {  // criar uma classe publica com o nome do programa
+    public static void main(String[] args) { // criar uma classe principal para rodar o programa
+        // abaixo vem o código
+        
+        System.out.println("Hello world!!!");
+    }
+}
