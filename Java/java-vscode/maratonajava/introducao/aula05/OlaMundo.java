8329714d67cceb4e8fa981eb6be642160cb46789
Lucas <lucsousan@gmail.com>
2020-10-25 16:10:04 -0300

    vai lá meu bom

---
+++ Java/java-vscode/maratonajava/introducao/aula05/OlaMundo.java
@@ -0,0 +1,18 @@
+package maratonajava.introducao.aula05;
+
+/* A primeira letra da classe em java sempre começa com a primeira letra em maiuscula quando a palavra é
+    simples. Se a palavra for composta: as duas palavras devem ser únidas, formando apenas uma; e a primeira
+    letra de cada palavra deve começar com letra mais maiuscula.
+    ex:
+        palavra         classe
+        casa            Casa
+        ola mundo       OlaMundo
+    Outro detalhe não colocara acento ou caracterer especial.
+    
+    */
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        
+    }    
+}
