24e9549a38b3363ec67f4f405c6b400deaddcd88
Lucas <lucsousan@gmail.com>
2020-10-25 20:30:47 -0300

    aula 07

---
+++ Java/java-vscode/maratonajava/introducao/aula007/operadores.java
@@ -0,0 +1,13 @@
+package maratonajava.introducao.aula007;
+
+public class operadores {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+        int resto_divisao;
+
+        resto_divisao = num1 % num2;
+
+        System.out.println("O Resto da divisão de num1 e num2 é " + resto_divisao);
+    }
+}
