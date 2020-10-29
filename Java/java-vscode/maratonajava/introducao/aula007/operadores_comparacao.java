c33e9fe082aeb01df6137c81508b2006840bd60b
Lucas <lucsousan@gmail.com>
2020-10-29 16:32:05 -0300

    final

---
+++ Java/java-vscode/maratonajava/introducao/aula007/operadores_comparacao.java
@@ -0,0 +1,9 @@
+package maratonajava.introducao.aula007;
+
+public class operadores_comparacao {
+    public static void main(String[] args) {
+        boolean dezMaiorQueVinte = 10 > 20;
+
+        System.out.println(dezMaiorQueVinte);
+    }
+}
