b63ac5ba04552af0e5e8ee8397dc6c44c6fc27eb
Lucas <lucsousan@gmail.com>
2020-10-30 00:31:33 -0300

    subindo

---
+++ Java/maratonajava/src/codigos/test/CarroTest.java
@@ -0,0 +1,11 @@
+package codigos.test;
+
+import maratonajavanovo.classes.Carro;
+
+public class CarroTest {
+
+    public static void main(String[] args) {
+        Carro C1 = new Carro();
+    }
+    
+}
