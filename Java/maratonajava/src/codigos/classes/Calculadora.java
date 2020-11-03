82479fd5808a68a27732c427d56f051eec4e345d
Lucas <lucsousan@gmail.com>
2020-11-03 11:23:11 -0300

    novo

--- Java/maratonajava/src/codigos/classes/Calculadora.java
+++ Java/maratonajava/src/codigos/classes/Calculadora.java
@@ -1,14 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package codigos.classes;
 
-/**
- *
- * @author lucas
- */
 public class Calculadora {
+        
+    // Métodos que não retornam nada da aula 27
     
+    public void somaDoisNumero(){
+        System.out.println("5 + 2 = " + (5 + 2));
+    }
+    
+    public void subtraiDoisNumero(){
+        System.out.println("5 - 2 = " + (5 - 2));
+    }
+    
+    public void multiplicaDoisNumero(){
+        System.out.println("5 * 2 = " + (5 * 2));
+    }
+    
+    public void dividiDoisNumero(){
+        System.out.println("5 / 2 = " + (5 / 2));
+    }
+    // Aula 27 termina Aqui
+    
+    /***
+     * Aula 28: Passar Parâmetro para os métodos
+     * @param num1  primeiro parâmetro que o método vai receber
+     * @param num2  segundo parâmetro que o método vai receber
+     *  esse método não retorna nenhum valor, mas imprime a divisão de num1 por num2.
+     */
+    public void multiplicaDoisNumero(int num1, int num2){
+        int produto; // produto vai receber a multiplicação de num1 e num 2
+        produto = num1 * num2;
+        System.out.println(num1 + " * " + num2 + " = " + produto);        
+    }
+    
+    /***
+     * Aula 29: Retornar valores atráves dos métodos
+     * @param num1 primeiro parametro
+     * @param num2 segundo parametro
+     * @return  retorno da divisao de num1 por num2
+     */
+    public double dividaDoisNumeros(double num1, double num2){
+        if(num2 != 0){
+            return num1 / num2;
+        
+        }else{
+            System.out.println("Operaçao INVALIDA!!!");
+            return 0;         
+        }
+    }
+    
+    public void alteraDoisNumeros(int num1, int num2){
+        num1 = 30;
+        num2 = 40;
+        System.out.println("Dentro do altera dois número");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+    }
 }
--- Java/maratonajava/src/codigos/test/CalculadoraTest.java
+++ Java/maratonajava/src/codigos/test/CalculadoraTest.java
@@ -1,21 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package codigos.test;
 
-/**
- *
- * @author lucas
- */
+import codigos.classes.Calculadora;
+
 public class CalculadoraTest {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+            
+        Calculadora c1 = new Calculadora();
+        
+        
+        // método da aula 27
+        System.out.println("=== Aula 27 ===");
+        c1.somaDoisNumero();
+        c1.subtraiDoisNumero();
+        c1.multiplicaDoisNumero();
+        c1.dividiDoisNumero();
+        
+        System.out.println(" ");
+        // método da aula 28
+        System.out.println("=== Aula 28 ===");
+        c1.multiplicaDoisNumero(4, 6);
+        
+        System.out.println(" ");
+        // método da aula 29
+        System.out.println("=== Aula 29 ===");
+        double res = c1.dividaDoisNumeros(8, 2);
+        System.out.println("O resultado da divisao é " + res);
+        
+        // método da aula 30
+        System.out.println("=== Aula 30 ===");
+        double res0 = c1.dividaDoisNumeros(14, 0);
+        System.out.println(res0);
+        
+         // método da aula 31
+         System.out.println("=== Aula 31 ===");
     }
     
 }
